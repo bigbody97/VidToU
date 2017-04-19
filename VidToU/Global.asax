6dc0505b1a8b0ff43fc20d27056b5f8d59390d1c
bigbody97 <famback17@gmail.com>
2017-04-18 23:22:58 -0700

    Add project files.

---
+++ VidToU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidToU.MvcApplication" Language="C#" %>
---
+++ VidToU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidToU.Startup))]
+namespace VidToU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
